0d53b0750668ce9baff18b5f53c744c98472f068
Larko_0 <larionov1001@gmail.com>
2020-06-12 18:18:52 +0300

    thirth lesson

--- find4names.py
+++ find4names.py
@@ -9,12 +9,24 @@ def get_robberies():
                 'город': robbery_list[1],
                 'жертва': robbery_list[2],
                 'украл': ukral_string_to_dict(robbery_list[3]),
-                'дата': robbery_list[4],
+                'дата': transform_string_date_to_list(robbery_list[4]),
                 'пойман': robbery_list[5] == 'T',
                 'речь': robbery_list[6]
             }
             robberies.append(robbery_dict)
     return robberies
+
+
+def transform_string_date_to_list(string):
+    """
+    :param string: "11-11-2132"
+    :return: [11, 11, 2132]
+    """
+    lst = string.split('-')  # ['11', '11', '2132']
+    new_list = []
+    for str_num in lst:
+        new_list.append(int(str_num))
+    return new_list
 
 
 def ukral_string_to_dict(string):
@@ -39,7 +51,7 @@ def ukral_string_to_dict(string):
     return thefts_dict
 
 
-def find_konfetolub():
+def find_konfetolub():  # Лопаточка
     robberies = get_robberies()
     thieves_counts = {}
     for robbery in robberies:
@@ -61,4 +73,170 @@ def find_konfetolub():
     print(f"{max_name} : {max_count}")
 
 
-find_konfetolub()
+def find_city():  # Здесьтаун
+    robberies = get_robberies()
+    cities_counts = {}
+    for robbery in robberies:
+        if robbery['город'] == 'Джастленд':
+            continue
+        if robbery['город'] not in cities_counts:
+            cities_counts[robbery['город']] = 0
+
+        cities_counts[robbery['город']] += 1
+
+    max_city = ""
+    max_count = 0
+    for city in cities_counts:
+        if max_count < cities_counts[city]:
+            max_count = cities_counts[city]
+            max_city = city
+
+    print(cities_counts)
+    print(max_city)
+
+
+def find_nuker():  # Воробоб
+    robberies = get_robberies()
+    thieves_counts = {}
+    for robbery in robberies:
+        if robbery['пойман']:
+            count = count_nu_in_string(robbery['речь'])
+            if robbery['имя'] not in thieves_counts:
+                thieves_counts[robbery['имя']] = 0
+            thieves_counts[robbery['имя']] += count
+
+    print(thieves_counts)
+    for thief in thieves_counts:
+        if thieves_counts[thief] > 5:
+            print(thieves_counts[thief], thief)
+
+
+def count_nu_in_string(string):
+    """
+    "Ну нуно не винувен ну..." -> 2
+    """
+    count = 0
+    words = string.split(' ')  # ['Ну', "нуно", "не", "винувен", "ну..."]
+    for word in words:
+        clear_word = word_cleaner(word)
+        if clear_word == 'ну':
+            count += 1
+
+    return count
+
+
+def word_cleaner(word):  # "Ну..!,?%" -> "ну"
+    """
+    .НУБЫвтфывФЫВФвфыПФПРФУ!"!№
+    нубывтфывывывфыпфпрфу
+    """
+    clear_word = ""
+    for symbol in word:
+        if symbol.isalpha():
+            clear_word += symbol.lower()
+    return clear_word
+
+
+def packer(robberies):
+    """
+    :param robberies: список ограблений
+    :return: упакованный словарь с ворами и списками их дат
+    {
+    'Bob': [[12, 23, 1552], [14, 29, 1625]],
+    'Boba': [...],
+    ...
+    }
+    """
+    thieves_dates = {}
+    for robbery in robberies:
+        if not robbery['пойман']:
+            date = robbery['дата']
+            name = robbery['имя']
+            if name not in thieves_dates:
+                thieves_dates[name] = []
+
+            thieves_dates[name].append(date)
+    return thieves_dates
+
+
+def find_4_month_thief():  # Копырсанка
+    robberies = get_robberies()
+    thieves_dates = packer(robberies)
+    for thief in thieves_dates:
+        dates = thieves_dates[thief]
+        sort_dates(dates)
+        max_seria = find_max_seria(dates)
+        if max_seria == 4:
+            print(thief)
+
+
+def sort_dates(dates):
+    ogr = len(dates) - 1
+    while ogr > 0:
+        i = 0
+        while i < ogr:
+            if is_date_bigger(dates[i], dates[i+1]):
+                temp = dates[i]
+                dates[i] = dates[i + 1]
+                dates[i + 1] = temp
+            i += 1
+        ogr -= 1
+
+
+def is_date_bigger(date1, date2):
+    """
+    :param date1: [1, 4, 2020]
+    :param date2: [12, 6, 2018]
+    :return: True
+    """
+    i = 2
+    while i >= 0:
+        if date1[i] > date2[i]:
+            return True
+        if date1[i] < date2[i]:
+            return False
+        i -= 1
+
+    return False
+
+
+def testcase1():
+    d = [[1, 14, 2020], [1, 11, 2030], [1, 11, 2020]]
+    sort_dates(d)
+    print(d)
+
+
+def razryv(date1, date2):
+    """
+    date2 > date1
+    :param date1:
+    :param date2:
+    :return:
+    """
+    years_dif = date2[2] - date1[2]  # 0
+    month_dif = date2[1] - date1[1]  # 3
+    dif = years_dif * 12 + month_dif
+    return dif
+
+
+def find_max_seria(dates):
+    i = 1
+    m = 1
+    max_ = 0
+    while i < len(dates):
+        dif = razryv(dates[i - 1], dates[i])
+        if dif == 1:
+            m += 1
+        else:
+            if m > max_:
+                max_ = m
+                m = 1
+        i += 1
+
+    if m > max_:
+        max_ = m
+
+    return max_
+
+
+find_4_month_thief()
